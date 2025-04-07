81d9d985beb6fd5385b27a425e9bb4fa76829299
vinicius.157 <vinicius.s.monteiro@aluno.senai.br>
2025-04-07 14:41:45 -0300

    Update vinicius.py
    ERROU!!!!!!!

--- vinicius.py
+++ vinicius.py
@@ -6,7 +6,7 @@ def main():
     loja.adicionar_carro(Carro("SW4", "Toyota", 2022, 240000.00, 20))
     
     while True:
-        print("Bem-vindo à loja de carros!")
+        print("Bem-vindo à loja ao hospicio!")
         print("1. Listar shorts")
         print("2. Adicionar produto ao carrinho")
         print("3. Ver carrinho")
@@ -18,7 +18,7 @@ def main():
         if opcao == "1":
             loja.listar_carros()
         elif opcao == "2":
-            modelo_carro = input("Digite o modelo do carro: ")
+            modelo_carro = input("Digite o modelo da arma: ")
             quantidade = int(input("Digite a quantidade: "))
             loja.adicionar_ao_carrinho(modelo_carro, quantidade)
         elif opcao == "3":
@@ -26,7 +26,7 @@ def main():
         elif opcao == "4":
             loja.realizar_compra()
         elif opcao == "5":
-            print("Obrigado por visitar nossa loja de carros!")
+            print("Obrigado por visitar nossa loja de caixões!")
             break
         else:
             print("Opção inválida. Tente novamente.")
