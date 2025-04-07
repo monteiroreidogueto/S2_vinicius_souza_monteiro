e599b63eb74f319f313ea48ef91e7a38601c8b4a
vinicius.157 <vinicius.s.monteiro@aluno.senai.br>
2025-04-07 14:33:02 -0300

    Update vinicius.py
    ERROU!!!!!!!

--- vinicius.py
+++ vinicius.py
@@ -1,21 +1,20 @@
 def main():
-    
     loja.adicionar_carro(Carro("Corolla Cros", "Toyota", 2020, 150000.00, 20))
     loja.adicionar_carro(Carro("Onix premier", "Chevrolet", 2024, 90000.00, 10))
     loja.adicionar_carro(Carro("Q7", "Audi", 2023, 220000.00, 15))
     loja.adicionar_carro(Carro("SW4", "Toyota", 2022, 240000.00, 20))
     
     while True:
-        print("Bem-vindo à loja de carros!")
+      
         print("1. Listar shorts")
         print("2. Adicionar produto ao carrinho")
-        print("3. Ver carrinho")
+        
         print("4. Realizar compra")
         print("5. Sair")
 
         opcao = input("Escolha uma opção (1-5): ")
 
-        if opcao == "1":
+        
             loja.listar_carros()
         elif opcao == "2":
             modelo_carro = input("Digite o modelo do carro: ")
